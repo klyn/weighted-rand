12d5870cd3bfc1106d905633b372174284995827
klyn <midtone.drama@gmail.com>
2024-11-05 20:51:46 +0400

    added a helper assertType function

--- src/__tests__/utils.ts
+++ src/__tests__/utils.ts
@@ -1,4 +1,4 @@
-import { isObject } from "../utils";
+import { isObject, assertType } from "../utils";
 
 // -- isObject
 test("isObject(x) should only return true for data objects", () => {
@@ -13,4 +13,17 @@ test("isObject(x) should only return true for data objects", () => {
   expect(isObject(class x {})).toBe(false); // class
   // success cases
   expect(isObject({})).toBe(true); // object
+});
+
+// -- assertType
+test("assertType should only accept strings for the second argument", () => {
+  const result = () => assertType(123, Number as any);
+  expect(result).toThrow(TypeError);
+});
+
+test("assertType should correctly throw an exception", () => {
+  expect(() => assertType(123, "string")).toThrow(TypeError);
+  expect(() => assertType("123", "number")).toThrow(TypeError);
+  expect(() => assertType("123", "function")).toThrow(TypeError);
+  expect(() => assertType("123", "object")).toThrow(TypeError);
 });
--- src/utils.ts
+++ src/utils.ts
@@ -25,3 +25,39 @@ export function isObject(x: any): boolean {
 
   return true;
 }
+
+/**
+ * Simple assert function to check whether a value is of an
+ * expected type or not.
+ *
+ * @param {any} thing - The value we like to check the type of
+ * @param {string} expectedType - The typeof operator result for
+ * the expected type
+ * @returns {never|undefined} - Throws an exception, thus, returning
+ * never in case of failure, otherwise it will return undefined
+ */
+export function assertType(
+  thing: any,
+  expectedType: string
+): never | undefined {
+  // - type of the value that we are checking
+  const typeOfThing = typeof thing;
+
+  // - the expectedType should always be a string
+  if (typeof expectedType !== "string") {
+    assertType(expectedType, "string");
+  }
+
+  // - check if the type is what we expect and like
+  if (typeOfThing !== expectedType) {
+    // - string template for the error message
+    const errorMessage = `Was expecting a '${expectedType}', but got a '${typeOfThing}' instead.`;
+    // - throw an exception
+    throw new TypeError(errorMessage);
+  }
+
+  // - the type of thing is satisfactory, no need for any specific
+  //   return values. returing undefined here just for the sake of
+  //   clarity.
+  return undefined;
+}
